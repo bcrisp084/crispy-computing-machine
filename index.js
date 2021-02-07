ba7d79fcb76cacee5ba1a0f1f6f80e009d249ec2
Brian crisp <bcrisp084@yahoo.com>
2021-02-07 14:03:18 -0500

    created question list and table of contents

--- index.js
+++ index.js
@@ -1,50 +1,108 @@
-import { prompt } from "inquirer";
-import { writeFile } from "fs";
+const inquirer = require("inquirer");
+const fs = require("fs");
 const axios = require("axios").default;
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-prompt([
-  {
-    type: "input",
-    message: "What is your user name?",
-    name: "username",
-  },
-  {
-    type: "input",
-    message: "What is your location?",
-    name: "Location",
-  },
-  {
-    type: "input",
-    message: "Enter a bio about yourself:",
-    name: "bio",
-  },
-  {
-    type: "input",
-    message: "Enter your linkedin url:",
-    name: "Linkedin",
-  },
-  {
-    type: "input",
-    message: "Enter your GitHub url:",
-    name: "GitHub",
-  },
-]).then((response) => {
-  // const data = response;
-  writeFile(`index.html`, buildHTML(response), (err) =>
-    err ? console.log(err) : console.log("sucess")
-  );
-});
+function promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "userName",
+    },
+    {
+      type: "input",
+      message: "What is your Project title?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "What is the description of your project?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter the installation instructions for your project",
+      name: "instructions",
+    },
+    {
+      type: "input",
+      message: "Enter your the application usage information",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Contributors to this project",
+      name: "contributions",
+    },
+    {
+      type: "input",
+      message: "Enter the project test information",
+      name: "testing",
+    },
+    {
+      type: "input",
+      message: "Please choose a license for your project",
+      name: "license",
+      choices: [
+        "Apache",
+        "MIT",
+        "Academic Free License v3.0",
+        "Open Software License 3.0",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+};
 
-// TODO: Include packages needed for this application
+getUserData = async (answers) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.github.com/search/repositories?q=repo:${answers.username}/${answers.repo}`
+    );
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function createMarkDown(answers) {
+  return `
+  // <img alt="NodeJS" src="https://img.shields.io/badge/node.js%20-%2343853D.svg?&style=for-the-badge&logo=node.js&logoColor=white"/>
+  # ${answers.username}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  ## Project Title
+  ${answers.ProjectTitle}
 
-// TODO: Create a function to initialize app
-function init() {}
+  ## Table of contents
+  + [Description](#description)
 
-// Function call to initialize app
-init();
+  + [Instructions](#instructions)
+
+  + [Usage](#usage)
+
+  + [Contributions](#contributions)
+
+  + [Testing](#testing)
+
+  + [License](#license)
+
+  + [Email](#email)
+
+  ## Instructions
+
+
+
+  
+  
+  
+  
+  
+  
+  `
+}
